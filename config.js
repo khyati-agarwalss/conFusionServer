d53bf9db6c1d540b4702d850e26e41fe85f76ad2
Khyati Agarwal <khyati.agarwalss@gmail.com>
2020-04-30 14:34:30 +0530

    OAuth with Passport and Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '622310428321443',
+        clientSecret: '351c3e179101373a9f8b571b861a2546'
+    }
 }
